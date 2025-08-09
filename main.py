925a82477a0c457267f7eae2944a4f02228799bd
Polina Belaya <polinabeline@gmail.com>
2025-08-09 22:54:59 +0300

    черновик

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 import streamlit as st
+from gigachat_api import get_access_token, send_prompt
 
-from gigachat_api import get_access_token, send_prompt, sent_prompt_and_get_response
 
-st.title("PoliteGPT")
+st.title('PoliteGPT')
+
 
 if "access_token" not in st.session_state:
     try:
         st.session_state.access_token = get_access_token()
-        st.toast("Получил токен")
     except Exception as e:
-        st.toast(f"Не получилось получить токен: {e}")
+        st.toast(e)
 
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "ai", "content": "Добрый день, человек. Развеешь мою скуку парой вопросов?"}]
+    st.session_state.messages = [{'role': 'ai', 'content':
+        'Добрый день. Ты можешь интересоваться всем, чем угодно. Главное, будь вежлив :) ...'}]
 
 for msg in st.session_state.messages:
-    if msg.get("is_image"):
-        st.chat_message(msg["role"]).image(msg["content"])
-    else:
-        st.chat_message(msg["role"]).write(msg["content"])
+    st.chat_message(msg['role']).write(msg['content'])
 
 if user_prompt := st.chat_input():
-    st.chat_message("user").write(user_prompt)
-    st.session_state.messages.append({"role": "user", "content": user_prompt})
+    st.chat_message('user').write(user_prompt)
+    st.session_state.messages.append({'role': 'user', 'content': user_prompt})
 
-    with st.spinner("Думаю..."):
-        response, is_image = sent_prompt_and_get_response(user_prompt, st.session_state.access_token)
-        if is_image:
-            st.chat_message("ai").image(response)
-            st.session_state.messages.append({"role": "ai", "content": response, "is_image": True})
+    with st.spinner('Думаю...'):
+        # Безопасный доступ к токену:
+        access_token = st.session_state.get("access_token", None)
+        if access_token is not None:
+            response = send_prompt(user_prompt, access_token)
+            st.session_state.messages.append({'role': 'ai', 'content': response})
+            st.chat_message('ai').write(response)
         else:
-            st.chat_message("ai").write(response)
-            st.session_state.messages.append({"role": "ai", "content": response})
+            st.toast("Ошибка: отсутствует access_token, попробуйте перезапустить приложение.")
